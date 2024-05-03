7bd6b678b92e3cca94eb5be9864d1ac1b564ea33
raelrzd <israelrezendeluiz2903@gmail.com>
2024-05-03 10:33:11 -0300

    Implementada dependencia, classe e primeiro teste de Navigation no projeto.

---
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -0,0 +1,37 @@
+package br.com.alura.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import br.com.alura.panucci.navigation.PanucciNavHost
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        composeTestRule.onRoot().printToLog("PanucciApp")
+        composeTestRule.onNodeWithText("Destaques do dia").assertIsDisplayed()
+    }
+
+}
