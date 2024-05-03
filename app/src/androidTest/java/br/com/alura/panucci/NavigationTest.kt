f21d1d5912fe34213f39d9a508864e2cd2c9865b
raelrzd <israelrezendeluiz2903@gmail.com>
2024-05-03 11:28:30 -0300

    Refatorado código da MainActivity para que seja possível acessar todo conteudo do Scaffold dentro do ambiente de testes. Também foi adicionado teste de validação de navegação e multiplos items na tela.

--- app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -1,14 +1,18 @@
 package br.com.alura.panucci
 
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import br.com.alura.panucci.navigation.PanucciNavHost
+import br.com.alura.panucci.navigation.menuRoute
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -24,7 +28,7 @@ class NavigationTest {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(ComposeNavigator())
-            PanucciNavHost(navController = navController)
+            PanucciApp(navController = navController)
         }
     }
 
@@ -32,6 +36,17 @@ class NavigationTest {
     fun appNavHost_verifyStartDestination() {
         composeTestRule.onRoot().printToLog("PanucciApp")
         composeTestRule.onNodeWithText("Destaques do dia").assertIsDisplayed()
+    }
+
+    @Test
+    fun appNavHost_verifyNavigateToMenuScreen() {
+        composeTestRule.onRoot().printToLog("PanucciApp")
+        composeTestRule.onNodeWithText("Menu").assertIsDisplayed().performClick()
+
+        composeTestRule.onAllNodesWithText("Menu").assertCountEquals(2)
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, menuRoute)
     }
 
 }
